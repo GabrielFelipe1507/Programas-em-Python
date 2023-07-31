b9db96c1827a9b9abc78acbe8e36c36169043a42
Gabriel Felipe 1507 <gabriel_felipe_trabalho@hotmail.com>
2023-07-31 12:20:45 -0300

    atualização 1.0

---
+++ tipos_de_operadores.py
@@ -0,0 +1,33 @@
+print("exemplo1: operadores aritméticos\n");
+produto1 = 20;
+produto2 = 10;
+
+print("operações:");
+print("soma=", produto1+produto2);
+print("subtração=", produto1-produto2);
+print("divisão=", produto1/produto2);
+print("divisão inteira=", produto1//produto2);
+print("multiplicação=", produto1*produto2);
+print("resto da divisao=", produto1%produto2);
+print("exponenciação=", produto1**produto2);
+
+print("exemplo2: operadores de comparação\n");
+
+saldo = 40;
+saque = 20;
+
+print("igual:", saldo== saque);
+print("diferente:", saldo!=saque);
+print("saldo maior?", saldo> saque);
+print("saldo menor?", saldo<saque);
+print("saldo maior igual?", saldo>=saque);
+print("saldo menor igual?", saldo<=saque);
+
+print("exemplo3: Operadores de atribuição:\n");
+
+
+
+
+
+
+
